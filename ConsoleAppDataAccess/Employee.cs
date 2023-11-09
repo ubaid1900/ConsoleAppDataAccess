449113bca7d36e5c022d3a78251d4a940deabde5
Safimohammed <Safi_mohd_2000@yahoo.com>
2023-11-09 10:31:54 +0530

    Delete Person

---
+++ ConsoleAppDataAccess/Employee.cs
@@ -0,0 +1,110 @@
+﻿using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using ConsoleAppDataAccess;
+
+
+namespace ConsoleAppEmpDataAccess
+{
+
+    public class Employee
+    {
+        public static List<Employee> GetEmployee()
+        {
+            List<Employee> list = new List<Employee>();
+            using SqlConnection sqlConnection = new SqlConnection(@"Server=.\SQLEXPRESS; INTEGRATED SECURITY= TRUE; DATABASE=Employeedb");
+
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("select Id,Firstname,Middlename,Lastname,Phonenumber from Employee", sqlConnection);
+            object s = sqlCommand.ExecuteScalar();
+            Console.WriteLine("Executescalar");
+            Console.WriteLine(s);
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                Employee Emp = new Employee();
+                Emp.Id = reader.GetInt32(0);
+                Emp.Firstname = reader.GetString(1);
+                Emp.Middlename = reader.GetString(2);
+                Emp.Lastname = reader.GetString(3);
+                Emp.Phonenumber = reader.GetInt32(4);
+                list.Add(Emp);
+
+                Console.WriteLine(@"{0}{1}{2}{3}{4}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4));
+            }
+            return list;
+
+
+            sqlConnection.Close();
+        }
+        public static Employee GetEmployee(int id)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(@"Server=.\SQLEXPRESS; INTEGRATED SECURITY = TRUE; DATABASE=Employeedb");
+            {
+            sqlConnection.Open();
+            
+            SqlCommand sqlCommand = new SqlCommand($"Select Id,Firstname,Middlename,Lastname,phonenumber from Employee where Id={id}",sqlConnection);
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.Read())
+            {
+
+                Employee emp = new Employee();
+                    Console.WriteLine("iam in if function");
+                emp.Id = reader.GetInt32(0);
+                emp.Firstname = reader.GetString(1);
+                emp.Middlename = reader.GetString(2);
+                emp.Lastname = reader.GetString(3);
+                emp.Phonenumber = reader.GetInt32(4);
+                // list.Add(emp);
+
+                sqlConnection.Close();
+                return emp;
+            }
+
+            sqlConnection.Close();
+            return null;
+           // Console.WriteLine("{0}{1}{2}{3}{4}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
+
+            }
+
+
+        }
+
+        public static  AddPerson()
+        {
+
+            using SqlConnection sqlConnection = new SqlConnection(@"Server=.\SQLEXPRESS; INTEGRATED SECURITY= TRUE; DATABASE = Employeedb");
+            {
+                sqlConnection.Open();
+
+               string strcommand ="insert into Employee values ('"
+                    + Employee.Id +"','"
+                    + Employee.Firtname +"','" +
+                    + Employee.Middlename +"','"
+                    + Employee.Lastname +"','"
+                    + Employee.Phonenumber + "')";
+
+                SqlCommand sqlCommand = new SqlCommand(strcommand, sqlConnection);
+                Console.WriteLine("Execute Nonqurey");
+                int s = sqlCommand.ExecuteNonQuery();
+
+                sqlConnection.Close();
+                //return s;
+            }
+        }
+
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Middlename { get; set; }
+    public string Lastname { get; set; }
+    public int Phonenumber { get; set; }
+}
+
+}
+
+
--- ConsoleAppDataAccess/Program.cs
+++ ConsoleAppDataAccess/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using ConsoleAppEmpDataAccess;
+using System.Diagnostics;
 
 namespace ConsoleAppDataAccess
 {
@@ -7,52 +8,31 @@ namespace ConsoleAppDataAccess
         static void Main(string[] args)
         {
             //Person.GetPersons();
-            Person.GetPersons("n");
-
-            #region get person
-            //Person person1 = Person.GetPerson(1);
-            //if (person1 == null)
-            //{
-            //    Console.WriteLine("Person not found");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("{0} {1}", person1.Firstname, person1.Lastname);
-            //    Console.WriteLine("{1}, {0}", person1.Firstname, person1.Lastname);
-            //}
-
-            //Console.WriteLine();
-
-            //Person person10 = Person.GetPerson(10);
-            //if (person10 == null)
-            //{
-            //    Console.WriteLine("Person not found");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("{0} {1}", person10.Firstname, person10.Lastname);
-            //    Console.WriteLine("{1}, {0}", person10.Firstname, person10.Lastname);
-            //}
-
-            #endregion
-
-            #region add person
-            //Person newPerson = new Person();
-            //newPerson.Firstname = "Test";
-            //newPerson.Lastname = "Test";
-            //newPerson.Middlename = "Test";
-            //newPerson.PhoneNumber = 147258369;
+            // Person.GetPersons("n");
+            //Person.GetPersons();
+            // Person.GetPerson(2);
+            //Employee.GetEmployee();
+            //Employee.GetEmployee(1);
+            Person.Delete(1);
+           #region add person
+           /* Person newPerson = new Person();
+            newPerson.Id = 3;
+            newPerson.Firstname = "munna";
+            newPerson.Lastname = "saleem";
+            newPerson.Middlename = "kaleem";
             //newPerson.DateofBirth = DateTime.Now.AddYears(-20);
-            //int addResult = Person.AddPerson(newPerson);
-            //if (addResult == 1)
-            //{
-            //    Console.WriteLine("new person added");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("failure adding a new person");
-            //} 
-            #endregion
+            newPerson.Phonenumber = 147258369;
+            int addResult = Person.AddPerson(newPerson);
+            if (addResult == 1)
+                {
+                Console.WriteLine("new person added");
+            }
+            else
+            {
+               Console.WriteLine("failure adding a new person");
+           } */
+           #endregion
+
         }
     }
 
