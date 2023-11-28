1bd37ea1c2e596f8d0aaab762b40705176850d15
ubaid1900 <ubaid1900@gmail.com>
2023-11-28 11:09:12 +0530

    Unit Tests

---
+++ ConsoleAppDataAccess/ForUnitTests.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleAppDataAccess
+{
+    public enum Operations
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide
+    }
+    public class ForUnitTests
+    {
+        public static int Operation(int n1, int n2, Operations operation)
+        {
+            switch (operation)
+            {
+                case Operations.Plus:
+                    {
+                        long l = (long)n1 + (long)n2;
+                        if (l > int.MaxValue)
+                        {
+                            return int.MaxValue;
+                        }
+                        if (l < int.MinValue)
+                        {
+                            return int.MinValue;
+                        }
+                        return n1 + n2;
+                    }
+                case Operations.Minus:
+                    return n1 - n2;
+                case Operations.Multiply:
+                    return n1 * n2;
+                case Operations.Divide:
+                    return Math.DivRem(n1, n2).Quotient;
+                default:
+                    throw new ArgumentException(nameof(operation));
+            }
+        }
+
+        public int ANotherOperation()
+        {
+            return 0;
+        }
+    }
+
+}
---
+++ ConsoleAppDataAccessTests/ForUnitTestsTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDataAccess.Tests
+{
+    [TestClass()]
+    public class ForUnitTestsTests
+    {
+        [TestMethod()]
+        [DataRow(1, 2, Operations.Plus, 3)]
+        [DataRow(10, 20, Operations.Plus, 30)]
+        [DataRow(-10, -20, Operations.Plus, -30)]
+        [DataRow(45, 8800, Operations.Plus, 8845)]
+        [DataRow(int.MaxValue, int.MaxValue, Operations.Plus, int.MaxValue)]
+        [DataRow(int.MinValue, int.MinValue, Operations.Plus, int.MinValue)]
+        public void OperationTest(int n1, int n2, Operations operation, int expected)
+        {
+            // Act
+            int actual = ForUnitTests.Operation(n1, n2, operation);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[TestMethod()]
+        public void ANotherOperationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
